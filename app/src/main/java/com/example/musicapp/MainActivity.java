6a803fb28816ce056cd7245bb6ca6aa9412b1baf
Cosmo-c-bit <dung9v3@gmail.com>
2024-11-02 02:25:42 +0700

    tối ưu nhỏ cho app

--- app/src/main/java/com/example/musicapp/MainActivity.java
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -458,6 +458,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        stopMusic(); // Ensure music is stopped and resources are released
         LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
     }
 }
